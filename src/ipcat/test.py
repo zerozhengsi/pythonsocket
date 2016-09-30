5c3c2b0d733a3f7e317e74e8eafa93e8291334db
bigday <zerozhengsi@hotmail.com>
2016-09-30 18:02:42 +0800

    update test

--- src/ipcat/test.py
+++ src/ipcat/test.py
@@ -1,13 +1,24 @@
 #coding=utf-8
 import urllib2    
+import threading
   
-url = 'http://www.xici.net.co/nn/1'  
+url = 'http://192.168.229.251/test'  
   
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'    
   
-headers = { 'User-Agent' : user_agent }    
-req = urllib2.Request(url, None, headers)    
-response = urllib2.urlopen(req)    
-the_page = response.read()
-print the_page
+headers = { 'User-Agent' : user_agent }  
+def test():  
+    req = urllib2.Request(url, None, headers)    
+    response = urllib2.urlopen(req)    
+    the_page = response.read()
+    print the_page
+    
+all_thread = []
+for i in range(10):
+    t = threading.Thread(target=test)
+    all_thread.append(t)
+    t.start()
+    
+for t in all_thread:
+    t.join()
 
