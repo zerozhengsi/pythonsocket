bf5af8cbc5df63375e68518ef570477c2b0288ec
bigday <zerozhengsi@hotmail.com>
2016-10-12 15:18:37 +0800

    update tcp server

--- src/tcp/server/TcpServer.py
+++ src/tcp/server/TcpServer.py
@@ -6,6 +6,7 @@ Created on 2016年6月23日
 '''
 import socket
 import threading
+import struct
 
 bind_ip = "0.0.0.0"
 bind_port = 9999
@@ -20,8 +21,14 @@ print "[*] listening on %s%d" % (bind_ip,bind_port)
 
 def handle_client(client_socket):
     request = client_socket.recv(1024)
-    print "[*] received: %s" % request
-    client_socket.send("ACK!")
+    start,length,code = struct.unpack("<ci6s",request)
+    print "[*] received start: %s" % start
+    print "[*] received length: %s" % length
+    print "[*] received code: %s" % code
+#     print "[*] received message: %s" % message
+#     print "[*] received end: %s" % end
+#     print "[*] received: %s" % request
+    client_socket.send("ok!")
     client_socket.close()
 
 while True:
